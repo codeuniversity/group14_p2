2cc12e3e21a9fd0cd40427154165d88768177846
Unknown <gabriela.sikora@code.berlin>
2017-11-12 19:07:12 +0100

    Button added #2

--- se/PhotoSwiper/App.js
+++ se/PhotoSwiper/App.js
@@ -7,13 +7,18 @@
 // react-native run-ios
 
 import React, { Component } from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
 export default class Swiper extends Component {
   render() {
     return (
       <View>
       <Counter/>
+      <Button 
+        title="Don't touch!" 
+        onPress={() => {this.setState({value:10})}
+          }
+        />
       </View>
     );
   }
@@ -25,7 +30,7 @@ class Counter extends Component {
     this.state = {value:0}
   }
   render() {
-    let value = this.state.value; // TODO:
+    let value = this.state.value; 
     return (
         <Text style={style.valueStyle}>
         {value}
@@ -40,8 +45,8 @@ valueStyle: {
   textAlign: 'center',
   margin: 10,
   fontWeight: '600',
-  fontStyle: 'italic',
-  fontFamily: 'Times New Roman'
+  fontFamily: 'Times New Roman',
+  backgroundColor: '#12A4A4',  
 }
 }
 );
