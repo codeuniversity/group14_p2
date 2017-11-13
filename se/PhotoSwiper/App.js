49be72575830fe42b44e8e470365939084e17d17
Unknown <gabriela.sikora@code.berlin>
2017-11-13 18:43:51 +0100

    Added photos and connected them
    Photos are in an array and can be looked through

--- se/PhotoSwiper/App.js
+++ se/PhotoSwiper/App.js
@@ -7,12 +7,64 @@
 // react-native run-ios
 
 import React, { Component } from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, Image} from 'react-native';
 
 export default class Swiper extends Component {
   render() {
+    const sources=[require('./TestPhotos/Photo01.jpg'), 
+                   require('./TestPhotos/Photo02.jpg'), 
+                   require('./TestPhotos/Photo03.jpg'), 
+                   require('./TestPhotos/Photo04.jpg'), 
+                   require('./TestPhotos/Photo05.jpg'), 
+                   require('./TestPhotos/Photo06.jpg'),
+                   require('./TestPhotos/Photo07.jpg'), 
+                   require('./TestPhotos/Photo08.jpg'), 
+                   require('./TestPhotos/Photo09.jpg'), 
+                   require('./TestPhotos/Photo10.jpg'),
+                   require('./TestPhotos/Photo11.jpg'), 
+                   require('./TestPhotos/Photo12.jpg'), 
+                   require('./TestPhotos/Photo13.jpg'), 
+                   require('./TestPhotos/Photo14.jpg'), 
+                   require('./TestPhotos/Photo15.jpg'), 
+                   require('./TestPhotos/Photo16.jpg'), 
+                   require('./TestPhotos/Photo17.jpg'), 
+                   require('./TestPhotos/Photo18.jpg'), 
+                   require('./TestPhotos/Photo19.jpg'), 
+                   require('./TestPhotos/Photo20.jpg'), 
+                   require('./TestPhotos/Photo21.jpg'), 
+                   require('./TestPhotos/Photo22.jpg'), 
+                   require('./TestPhotos/Photo23.jpg'), 
+                   require('./TestPhotos/Photo24.jpg'), 
+                   require('./TestPhotos/Photo25.jpg'), 
+                   require('./TestPhotos/Photo26.jpg'), 
+                   require('./TestPhotos/Photo27.jpg'), 
+                   require('./TestPhotos/Photo28.jpg'), 
+                   require('./TestPhotos/Photo29.jpg'), 
+                   require('./TestPhotos/Photo30.jpg'),
+                   require('./TestPhotos/Photo31.jpg'),
+                   require('./TestPhotos/Photo32.jpg'),
+                   require('./TestPhotos/Photo33.jpg'),
+                   require('./TestPhotos/Photo34.jpg'),
+                   require('./TestPhotos/Photo35.jpg'),
+                   require('./TestPhotos/Photo36.jpg'),
+                   require('./TestPhotos/Photo37.jpg'),
+                   require('./TestPhotos/Photo38.jpg'),
+                   require('./TestPhotos/Photo39.jpg'),
+                   require('./TestPhotos/Photo40.jpg'),
+                   require('./TestPhotos/Photo41.png'),
+                   require('./TestPhotos/Photo42.jpg'),
+                   require('./TestPhotos/Photo43.png'),
+                   require('./TestPhotos/Photo44.jpg'),
+                   require('./TestPhotos/Photo45.jpg'),
+                   require('./TestPhotos/Photo46.jpg'),
+                   require('./TestPhotos/Photo47.jpg'),
+                   require('./TestPhotos/Photo48.jpg'),
+                   require('./TestPhotos/Photo49.jpg'),
+                   require('./TestPhotos/Photo50.jpg')]
+    
     return (
       <View>
+      <Image source={sources[1]} />
       <Counter/>
       </View>
     );
